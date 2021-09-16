a1122babc723c08378350dedee1187a74abc4fe6
SerafimArts <nesk@xakep.ru>
2021-09-16 18:32:10 +0300

    Remove RR 1.x worker from example

--- example/server/worker.php
+++ example/server/worker.php
@@ -5,8 +5,7 @@
  */
 
 use Service\EchoInterface;
-use Spiral\Goridge\StreamRelay;
-use Spiral\GRPC\Server;
+use Spiral\RoadRunner\GRPC\Server;
 use Spiral\RoadRunner\Worker;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -17,11 +16,4 @@ $server = new Server(null, [
 
 $server->registerService(EchoInterface::class, new EchoService());
 
-$worker = \method_exists(Worker::class, 'create')
-    // RoadRunner >= 2.x
-    ? Worker::create()
-    // RoadRunner 1.x
-    : new Worker(new StreamRelay(STDIN, STDOUT))
-;
-
-$server->serve($worker);
+$server->serve(Worker::create());
