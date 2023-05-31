98e0f416f633d7ab0afae970770a84d50b6dc69d
butschster <butschster@gmail.com>
2023-05-31 19:48:48 +0400

    Adds unit test with testing exception with details

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Spiral\RoadRunner\GRPC\Tests;
 
+use Google\Rpc\Status;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 use Service\Message;
 use Service\TestInterface;
 use Spiral\Goridge\Frame;
 use Spiral\Goridge\RelayInterface;
+use Spiral\RoadRunner\GRPC\Exception\ServiceException;
 use Spiral\RoadRunner\GRPC\Server;
 use Spiral\RoadRunner\GRPC\Tests\Stub\TestService;
+use Spiral\RoadRunner\Payload;
 use Spiral\RoadRunner\Worker;
+use Spiral\RoadRunner\WorkerInterface;
+use Spiral\RoadRunner\GRPC\InvokerInterface;
 
 class ServerTest extends TestCase
 {
@@ -40,7 +45,7 @@ class ServerTest extends TestCase
         );
 
         $relay->shouldReceive('send')->once()->withArgs(function (Frame $frame) {
-            return $frame->payload === '{}'.$this->packMessage('pong');
+            return $frame->payload === '{}' . $this->packMessage('pong');
         });
 
         $this->server->serve(
@@ -112,6 +117,39 @@ class ServerTest extends TestCase
         );
     }
 
+    public function testExceptionDetails(): void
+    {
+        $error = new Message();
+        $error->setMsg('Invalid sample id');
+
+        $invoker = m::mock(InvokerInterface::class);
+        $invoker->shouldReceive('invoke')->once()
+            ->andThrow(new ServiceException('Sample endpoint error', 200, [$error]));
+
+        $worker = m::mock(WorkerInterface::class);
+        $worker->shouldReceive('waitPayload')->once()
+            ->andReturn(new Payload(body: 'ping', header: '{"context": {}, "service": "service.Test", "method": "Throw"}'));
+
+        $worker->shouldReceive('waitPayload')->once()->andReturnNull();
+
+        $worker->shouldReceive('respond')->once()->withArgs(function (Payload $payload) {
+            $headers = json_decode($payload->header, true);
+            $status = new Status();
+            $status->mergeFromString(\base64_decode($headers['error']));
+
+            $message = $status->getDetails()->offsetGet(0);
+            $message = $message->unpack();
+
+            return $message instanceof Message && $message->getMsg() === 'Invalid sample id';
+        });
+
+        $server = new Server($invoker);
+        $service = new TestService();
+
+        $server->registerService(TestInterface::class, $service);
+        $server->serve($worker);
+    }
+
     private function packMessage(string $message): string
     {
         $m = new Message();
@@ -135,7 +173,7 @@ class ServerTest extends TestCase
 
         $relay = m::mock(RelayInterface::class);
         $relay->shouldReceive('waitFrame')->once()->andReturn(
-            new Frame($header.$body, [mb_strlen($header)])
+            new Frame($header . $body, [mb_strlen($header)])
         );
 
         $header = json_encode(['stop' => true]);
